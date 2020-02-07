484e683c3b6fc6f21ec0c371bcc6f3c27c7df14f
Philip Krause <pkrause@fl01mb6383.hci01.local>
2020-02-07 15:41:25 -0500

    fml

--- api.py
+++ api.py
@@ -4,38 +4,26 @@ import os
 
 
 api_token = os.environ['PINGDOM_TOKEN']
-api_url   = 'https://api.pingdom.com/api/3.1/checks'
+api_url   = "https://api.pingdom.com/api/3.1/checks"
 
-headers   = {
-            'Content-Type' : 'application/json',
-            'Authorization': 'Bearer {0}'.format(api_token) 
-            }
+auth = 'Bearer ' + api_token
 
-print(headers)
+head   = {
+            "Content-Type" : "application/json",
+            "Authorization": auth
+            }       
 
 def call_api():
 
-    response = requests.get(api_url, headers=headers)
+    response = requests.get(api_url, headers=head)
 
-    if response.status_code == 404:
-        print('[!] [{0}] URL not found: [{1}]'.format(response.status_code,api_url))
-        return None  
-    elif response.status_code == 401:
-        print('[!] [{0}] Authentication Failed'.format(response.status_code))
-        return None
-    elif response.status_code == 400:
-        print('[!] [{0}] Bad Request'.format(response.status_code))
-        return None
-    elif response.status_code >= 300:
-        print('[!] [{0}] Unexpected Redirect'.format(response.status_code))
-        return None
-    elif response.status_code == 200:
+    if response.status_code == 200:
         data = json.loads(response.content.decode('utf-8'))
+
+        print(data)
         return data
     else:
-        print('[?] Unexpected Error: [HTTP {0}]: Content: {1}'.format(response.status_code, response.content))
-    return None
-  
+        print("You suck at coding. Have Jen fix this.")
 
 call_api()  
 
