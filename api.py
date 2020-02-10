cc230cb93cf1867c063f6776048be52c76ad7f99
Philip Krause <pkrause@ip-10-240-0-1.ec2.internal>
2020-02-10 15:47:00 -0500

    working

--- api.py
+++ api.py
@@ -6,37 +6,28 @@ import os
 api_token = os.environ['PINGDOM_TOKEN']
 api_url   = 'https://api.pingdom.com/api/3.1/checks'
 
-
-head   = {
-            'Authorization': 'Bearer {0}'.format(api_token)
-         }       
+head   = {'Authorization': 'Bearer {0}'.format(api_token)}       
 
 def call_api():
 
     response = requests.get(api_url, headers=head)
 
-    if response.status_code >= 500:
-        print('[!] [{0}] Server Error'.format(response.status_code))
-        return None
-    elif response.status_code == 404:
-        print('[!] [{0}] URL not found: [{1}]'.format(response.status_code,api_url))
-        return None  
-    elif response.status_code == 401:
-        print('[!] [{0}] Authentication Failed'.format(response.status_code))
-        return None
-    elif response.status_code == 400:
-        print('[!] [{0}] Bad Request'.format(response.status_code))
-        return None
-    elif response.status_code >= 300:
-        print('[!] [{0}] Unexpected Redirect'.format(response.status_code))
-        return None
+    if   response.status_code != 200:
+        print('!!Error!! || StatusCode: {0}'.format(response.status_code))
+    
     elif response.status_code == 200:
         data = json.loads(response.content.decode('utf-8'))
-        print(data)
-        return data
+        
+        for i in range(len(data['checks'])):
+            hostname = data['checks'][i]['name']
+            idef = str(data['checks'][i]['id'])
+            status = data['checks'][i]['status']
+            print(f"Domain: {hostname} || ID: {idef} || Status: {status} ")
+
     else:
         print('[?] Unexpected Error: [HTTP {0}]: Content: {1}'.format(response.status_code, response.content))
-    return None
+
 
 call_api()  
+
 
