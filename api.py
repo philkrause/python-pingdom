fd00924331b15a854284fc459ac555785765db4c
Philip Krause <pkrause@ip-10-75-0-2.ec2.internal>
2020-02-11 09:03:45 -0500

    Added Hostname

--- api.py
+++ api.py
@@ -12,20 +12,19 @@ def call_api():
 
     response = requests.get(api_url, headers=head)
 
-    if   response.status_code != 200:
+    if  response.status_code != 200:
         print('!!Error!! || StatusCode: {0}'.format(response.status_code))
     
     elif response.status_code == 200:
         data = json.loads(response.content.decode('utf-8'))
         
-        for i in range(len(data['checks'])):
-            hostname = data['checks'][i]['name']
-            idef = str(data['checks'][i]['id'])
-            status = data['checks'][i]['status']
-            print(f"Domain: {hostname} || ID: {idef} || Status: {status} ")
-
-    else:
-        print('[?] Unexpected Error: [HTTP {0}]: Content: {1}'.format(response.status_code, response.content))
+    for i in range(len(data['checks'])):
+        name = data['checks'][i]['name']
+        idef = str(data['checks'][i]['id'])
+        status = data['checks'][i]['status']
+        hostname = data['checks'][i]['hostname']
+            
+        print(f"Name: {name} || ID: {idef} || Status: {status} || Hostname : {hostname} ")
 
 
 call_api()  
